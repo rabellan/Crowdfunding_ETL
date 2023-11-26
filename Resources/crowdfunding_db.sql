4239d29906ccab4587ad6071bf10d4b998432ec2
rabellan <roland.abellano@gmail.com>
2023-11-26 12:18:18 -0800

    updated README, uploaded schema and db sql files

---
+++ Resources/crowdfunding_db.sql
@@ -0,0 +1,11 @@
+-- Verify contacts
+SELECT * FROM contacts;
+
+-- Verify category
+SELECT * from category;
+
+-- Verify subcategory
+SELECT * FROM subcategory;
+
+-- Verify campaign
+SELECT * FROM campaign;
